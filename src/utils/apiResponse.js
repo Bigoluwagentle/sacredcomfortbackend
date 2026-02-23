a01f5d7902c336342d946064107f179aac62d95d
bigoluwagentle <olayiwolaolabode06@gmail.com>
2026-02-23 09:40:09 -0800

    apiResponse.js

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,15 @@
+export const successResponse = (res, statusCode = 200, message = 'Success', data = {}) => {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data,
+  });
+};
+
+export const errorResponse = (res, statusCode = 500, message = 'Something went wrong', errors = null) => {
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    ...(errors && { errors }),
+  });
+};
