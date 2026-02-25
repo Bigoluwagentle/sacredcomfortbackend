05a3ee70552f5e7fe97c6bb55b956b814a5064b1
bigoluwagentle <olayiwolaolabode06@gmail.com>
2026-02-25 08:35:15 -0800

    index.js

---
+++ src/models/postgres/index.js
@@ -0,0 +1,28 @@
+import sequelize from '../../config/db.postgres.js';
+
+import Quran from './Quran.model.js';
+import Hadith from './Hadith.model.js';
+import Bible from './Bible.model.js';
+import Philosophy from './Philosophy.model.js';
+import Therapist from './Therapist.model.js';
+import TherapistAvailability from './TherapistAvailability.model.js';
+import AudioFile from './AudioFile.model.js';
+import CrisisResource from './CrisisResource.model.js';
+
+Therapist.hasMany(TherapistAvailability, { foreignKey: 'therapistId', as: 'availability' });
+TherapistAvailability.belongsTo(Therapist, { foreignKey: 'therapistId' });
+
+Quran.hasMany(AudioFile, { foreignKey: 'verseId', constraints: false, scope: { religion: 'Islam' } });
+Bible.hasMany(AudioFile, { foreignKey: 'verseId', constraints: false, scope: { religion: 'Christianity' } });
+
+export {
+  sequelize,
+  Quran,
+  Hadith,
+  Bible,
+  Philosophy,
+  Therapist,
+  TherapistAvailability,
+  AudioFile,
+  CrisisResource,
+};
